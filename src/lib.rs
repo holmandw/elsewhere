5c04dcc9c0f10a97e39a1e1a342bde6ba5767057
Dustin Holman <github.com.sheet923@passmail.net>
2025-03-01 15:27:36 -0800

    lib working

---
+++ src/lib.rs
@@ -0,0 +1,173 @@
+use std::fs;
+use std::path::PathBuf;
+
+use chrono::Utc;
+use chrono_tz::Tz;
+use dirs::home_dir;
+use serde::{Deserialize, Serialize};
+
+static CONFIG_FILENAME: &'static str = ".ew.toml";
+static DEFAULT_STRFTIME: &'static str = "%H:%M %z    %a %b %d %Y";
+
+pub struct App {
+    config: TomlConfig,
+    config_file: PathBuf,
+}
+
+impl App {
+    /// Creates a default `App` instance with
+    /// the default file location and config values.
+    /// If it doesn't exist, this creates a file at
+    /// `$HOME/.ew.toml` and populates default values.
+    pub fn new() -> App {
+        let pb = Self::get_default_pb();
+        let config = TomlConfig::get_or_create(&pb);
+        App {
+            config,
+            config_file: pb,
+        }
+    }
+
+    /// Add a new person to the config.
+    pub fn add(&mut self, p: Person) {
+        self.config.add(p);
+        let toml_s = toml::to_string(&self.config).unwrap();
+        fs::write(&self.config_file, toml_s).unwrap();
+    }
+
+    pub fn rm(&mut self, name: &str) {
+        self.config.remove(name);
+        let toml_s = toml::to_string(&self.config).unwrap();
+        fs::write(&self.config_file, toml_s).unwrap();
+    }
+
+    /// Print out all of the entries. Self is mutably borrowed
+    /// so the vector can be sorted if applicable.
+    pub fn list(&mut self) {
+        let now = Utc::now();
+        let buff_size = self
+            .config
+            .person
+            .iter()
+            .map(|p| p.name.len())
+            .max()
+            .unwrap_or(0)
+            + 4;
+        if self.config.person.len() == 0 {
+            println!("no entries!");
+            return;
+        }
+
+        if self.config.should_sort() {
+            self.config.sort()
+        }
+
+        let fmt = self
+            .config
+            .date_fmt
+            .clone()
+            .unwrap_or((&DEFAULT_STRFTIME).to_string());
+
+        self.config.person.iter().for_each(|p| {
+            if let Ok::<Tz, _>(tz) = p.tz.parse() {
+                let now_local = now.with_timezone(&tz).format(&fmt);
+                println!("{:buff_size$}{}", p.name, now_local);
+            } else {
+                println!("invalid tz {} for {}", p.tz, p.name);
+            }
+        });
+    }
+
+    fn get_default_pb() -> PathBuf {
+        [home_dir().unwrap(), CONFIG_FILENAME.into()]
+            .iter()
+            .collect()
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct TomlConfig {
+    person: Vec<Person>,
+    date_fmt: Option<String>,
+    sort: Option<bool>,
+}
+
+impl TomlConfig {
+    fn empty() -> TomlConfig {
+        TomlConfig {
+            person: Vec::new(),
+            date_fmt: Some(DEFAULT_STRFTIME.into()),
+            sort: Some(true),
+        }
+    }
+
+    /// Either get the contents of a config or create an empty config.
+    pub fn get_or_create(pb: &PathBuf) -> TomlConfig {
+        match fs::exists(&pb) {
+            Ok(true) => {
+                let contents = fs::read_to_string(&pb).unwrap();
+                if let Ok(config) = toml::from_str(&contents) {
+                    config
+                } else {
+                    TomlConfig::empty()
+                }
+            }
+            Ok(false) => {
+                println!("writing default config to `{}`!", pb.display());
+                let config = TomlConfig::empty();
+                let s: String = toml::to_string(&config).unwrap();
+                fs::write(&pb, s).unwrap();
+                config
+            }
+            Err(err) => panic!("{err}"),
+        }
+    }
+
+    fn sort(&mut self) {
+        self.person.sort_by(|p1, p2| p1.name.cmp(&p2.name));
+    }
+
+    fn should_sort(&self) -> bool {
+        match self.sort {
+            Some(true) => true,
+            Some(false) => false,
+            None => true,
+        }
+    }
+
+    fn add(&mut self, p: Person) {
+        self.person.push(p);
+
+        if self.should_sort() {
+            self.sort();
+        }
+    }
+
+    fn remove(&mut self, name: &str) {
+        if let Some(idx) = self
+            .person
+            .iter()
+            .position(|p| p.name.to_lowercase() == name.to_lowercase())
+        {
+            self.person.swap_remove(idx);
+        }
+        if self.should_sort() {
+            self.sort();
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Person {
+    name: String,
+    tz: String,
+}
+
+impl Person {
+    pub fn new(name: &str, tz: &str) -> Person {
+        Person {
+            name: name.into(),
+            tz: tz.into(),
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,140 +1,6 @@
-use std::fs;
-use std::path::PathBuf;
-
-use chrono::Utc;
-use chrono_tz::Tz;
 use clap::{arg, Command};
-use dirs::home_dir;
-use serde::{Deserialize, Serialize};
 
-static CONFIG_FILENAME: &'static str = ".ew.toml";
-static DEFAULT_STRFTIME: &'static str = "%H:%M %z    %a %b %d %Y";
-
-#[derive(Serialize, Deserialize, Debug, Clone)]
-struct Config {
-    person: Vec<Person>,
-    date_fmt: Option<String>,
-}
-
-impl Config {
-    fn empty() -> Config {
-        Config {
-            person: Vec::new(),
-            date_fmt: Some(DEFAULT_STRFTIME.into()),
-        }
-    }
-
-    /// Either get the contents of a config or create an empty config.
-    fn get_or_create_config() -> Config {
-        let pb = get_pb();
-        match fs::exists(&pb) {
-            Ok(true) => {
-                let contents = fs::read_to_string(&pb).unwrap();
-                if let Ok(config) = toml::from_str(&contents) {
-                    config
-                } else {
-                    Config::empty()
-                }
-            }
-            Ok(false) => {
-                println!("writing default config to `{}`!", pb.display());
-                let config = Config::empty();
-                let s: String = toml::to_string(&config).unwrap();
-                fs::write(&pb, s).unwrap();
-                config
-            }
-            Err(err) => panic!("{err}"),
-        }
-    }
-
-    fn sorted(&self) -> Self {
-        let mut copy = self.clone();
-        copy.person.sort_by(|p1, p2| p1.name.cmp(&p2.name));
-        copy
-    }
-
-    fn add(&mut self, p: Person, sort: bool) {
-        self.person.push(p);
-        if sort {
-            self.person = self.sorted().person;
-        }
-    }
-
-    fn remove(&mut self, name: &str, sort: bool) {
-        if let Some(idx) = self
-            .person
-            .iter()
-            .position(|p| p.name.to_lowercase() == name.to_lowercase())
-        {
-            self.person.swap_remove(idx);
-        }
-        if sort {
-            self.person = self.sorted().person;
-        }
-    }
-}
-
-#[derive(Serialize, Deserialize, Debug, Clone)]
-struct Person {
-    name: String,
-    tz: String,
-}
-
-impl Person {
-    fn new(name: &str, tz: &str) -> Person {
-        Person {
-            name: name.into(),
-            tz: tz.into(),
-        }
-    }
-}
-
-fn get_pb() -> PathBuf {
-    [home_dir().unwrap(), CONFIG_FILENAME.into()]
-        .iter()
-        .collect()
-}
-
-fn add_person(person: Person, sort: bool) {
-    let mut config = Config::get_or_create_config();
-    config.add(person, sort);
-    let toml_s = toml::to_string(&config).unwrap();
-    let pb = get_pb();
-    fs::write(pb, toml_s).unwrap();
-}
-
-fn remove_by_name(name: &str, sort: bool) {
-    let mut config = Config::get_or_create_config();
-    config.remove(name, sort);
-    let toml_s = toml::to_string(&config).unwrap();
-    let pb = get_pb();
-    fs::write(pb, toml_s).unwrap();
-}
-
-fn run() {
-    let config = Config::get_or_create_config();
-    let now = Utc::now();
-    let buff_size = config
-        .person
-        .iter()
-        .map(|p| p.name.len())
-        .max()
-        .unwrap_or(0)
-        + 4;
-    if config.person.len() == 0 {
-        println!("no entries!");
-        return;
-    }
-    let fmt = config.date_fmt.unwrap_or((&DEFAULT_STRFTIME).to_string());
-    config.person.iter().for_each(|p| {
-        if let Ok::<Tz, _>(tz) = p.tz.parse() {
-            let now_local = now.with_timezone(&tz).format(&fmt);
-            println!("{:buff_size$}{}", p.name, now_local);
-        } else {
-            println!("invalid tz {} for {}", p.tz, p.name);
-        }
-    });
-}
+use ew::{App, Person};
 
 fn main() {
     let cmd = Command::new("ew")
@@ -159,15 +25,17 @@ fn main() {
                 .map(|s| s.as_str())
                 .zip(sub_matches.get_one::<String>("tz").map(|s| s.as_str()))
                 .map(|(n, t)| Person::new(n, t))
-                .map(|p| add_person(p, true));
+                .zip(Some(App::new()))
+                .map(|(p, mut app)| app.add(p));
         }
         Some(("rm", sub_matches)) => {
             sub_matches
                 .get_one::<String>("name")
                 .map(|s| s.as_str())
-                .map(|n| remove_by_name(n, true));
+                .zip(Some(App::new()))
+                .map(|(n, mut app)| app.rm(n));
         }
-        None => run(),
+        None => App::new().list(),
         _ => unreachable!(),
     };
 }
